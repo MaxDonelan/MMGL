8e2ffd00dce933b42c107fed92b575944d7727b9
MaxDonelan <maxdonelan154@gmail.com>
2026-05-28 14:23:43 -0500

    Initial working version

--- data/RADFUSION/hyperparameter_tuning.py
+++ data/RADFUSION/hyperparameter_tuning.py
@@ -1,4 +1,5 @@
 import random
+from functools import partial
 import numpy as np
 import torch
 from torch.utils.data import DataLoader
@@ -8,17 +9,16 @@ from hyperopt import hp, STATUS_OK
 from lung_detection import *
 
 
-def train_parameter_combo(batch_size, learning_rate, momentum):
-    samples = np.load("data/RADFUSION/labeling_samples.npy")
-    labels = np.load("data/RADFUSION/lung_presence_key.npy")
+def train_parameter_combo(params, X_train, X_val, y_train, y_val, X_test, y_test):
+    batch_sizes = [8, 16, 32, 64, 128, 256]
+    learning_rates = [0.0001, 0.001, 0.01, 0.1]
+    batch_size = batch_sizes[params["batch_size"]]
+    learning_rate = learning_rates[params["learning_rate"]]
+    momentum = params["momentum"]
+
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
-    for i in range(samples.shape[0]):
-        samples[i, :, :] = normalize_slice(samples[i, :, :])
-
-    X_train, X_test, y_train, y_test = train_test_split(samples, labels, stratify=labels, test_size=0.2)
-
-    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, stratify=y_train, test_size=0.125)
+    print(f"\nBatch Size: {batch_size} | Learning Rate: {learning_rate} | Momentum: {momentum}\n")
 
     train = SliceSample(X_train, y_train)
     val = SliceSample(X_val, y_val)
@@ -32,9 +32,10 @@ def train_parameter_combo(batch_size, learning_rate, momentum):
                             device=device)
     
     model_summary, cnn = model.best_fit(train_loader, val_loader, metric="loss")
-    val_loss = np.min(model_summary["test_loss"])
+    val_loss = np.min(model_summary["Test Loss"])
 
-    X_test = torch.Tensor(X_test).to(device)
+    X_test = torch.tensor(X_test, dtype=torch.float32).to(device).unsqueeze(1)
+    y_test = torch.tensor(y_test, dtype=torch.long).to(device)
     test_out = cnn(X_test)
     criterion = torch.nn.CrossEntropyLoss()
     test_loss = criterion(test_out, y_test).item()
@@ -47,10 +48,20 @@ def train_parameter_combo(batch_size, learning_rate, momentum):
 if __name__ == "__main__":
     random.seed(47)
 
-    best_choice = hyperopt.fmin(train_parameter_combo, 
+    samples = np.load("data/RADFUSION/labeling_samples.npy")
+    labels = np.load("data/RADFUSION/lung_presence_key.npy")
+    
+
+    for i in range(samples.shape[0]):
+        samples[i, :, :] = normalize_slice(samples[i, :, :])
+
+    X_train, X_test, y_train, y_test = train_test_split(samples, labels, stratify=labels, test_size=0.2, random_state=47)
+    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, stratify=y_train, test_size=0.125, random_state=47)
+
+    best_choice = hyperopt.fmin(partial(train_parameter_combo, X_train=X_train, X_val=X_val, y_train=y_train, y_val=y_val, X_test=X_test, y_test=y_test), 
                                 space={
-                                    "batch_size": hp.choice("batch_size", [8, 16, 32, 64, 128, 256]),
-                                    "learning_rate": hp.loguniform("learning_rate", 1e-4, 1e-1),
+                                    "batch_size": hp.randint("batch_size", 6),
+                                    "learning_rate": hp.randint("learning_rate", 4),
                                     "momentum": hp.uniform("momentum", 0, 1)
                                 },
                                 algo=hyperopt.tpe.suggest,
